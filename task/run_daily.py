e7eb4a49a6833146f24a47d9e7be58ec7a90637d
wsc7758 <757878213@qq.com>
2026-07-22 12:34:35 +0800

    Update run_daily.py

--- task/run_daily.py
+++ task/run_daily.py
@@ -1,11 +1,9 @@
-"""
-GitHub Actions 每日任务入口
-"""
 from datetime import date
 import chinese_calendar
 from exporter.md_exporter import export_md
 from exporter.json_exporter import export_json
 from core.sector_hot_engine import analyze_hot_sector
+from data.datasource import get_sector_stock
 from core.stock_rank_engine import get_sector_leaders
 
 DISCLAIMER = """
@@ -17,15 +15,15 @@ DISCLAIMER = """
 
 if __name__ == "__main__":
     print(DISCLAIMER)
-    # 判断A股交易日，休市直接退出
+    # 判断A股交易日
     if not chinese_calendar.is_workday(date.today()):
         print("当前非A股交易日，程序结束。")
         exit(0)
 
-    print("开始执行：热点板块分析")
+    print("开始执行：热点板块分析任务")
     hot_sector_df, sentiment_info = analyze_hot_sector()
     if hot_sector_df.empty:
-        print("未筛选出达标热点板块，任务结束")
+        print("未筛选出满足阈值的热点板块，任务结束")
         exit()
 
     final_result = []
@@ -39,10 +37,12 @@ if __name__ == "__main__":
         final_result.append({
             "sector_name": sec_name,
             "total_score": sec_row["total_score"],
+            "main_fund_flow": sec_row["main_fund_flow"],
+            "sector_change": sec_row["sector_change"],
             "leaders": leader_list
         })
 
-    # 导出文件
+    # 输出报告
     export_md(final_result, sentiment_info)
-    export_json(final_result)
-    print("✅ 选股任务执行完成！")
+    export_json(final_result, sentiment_info)
+    print("✅ 选股任务全部执行完成！结果已保存")
